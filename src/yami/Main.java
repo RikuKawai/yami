1920b91f41f1bb30da1359b7207de1da50342cd5
RikuKawai <riku@thisis.moe>
2016-04-11 14:34:56 -0400

    initial story

--- src/yami/Main.java
+++ src/yami/Main.java
@@ -1,12 +1,18 @@
 package yami;
 
-
+import hsa_new.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// yami prototype
+		Console c = new Console(30,100,11,"yami");
+		c.println("You awaken in what appears to be a cave, you are tired, hungry, and it's very dark.");
+		c.println("In the distance you can see a faint light, behind you the cave heads deeper.");
+		c.println();
+		c.print("Head [deeper] or follow the [light]?");
+		
 	}
 
 }
